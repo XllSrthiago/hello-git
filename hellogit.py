e55d5d75e819d13cf09d785f05599f7ea7531fc6
SantiTriana <Santi239xll@gmail.com>
2023-03-02 14:12:35 -0500

    hellogit.py

--- hellogit.py
+++ hellogit.py
@@ -2,8 +2,11 @@ print("New Hello Git")
 
 # git init Para iniciar
 # git add Agrega el contenido del archivo al índice
-# git commit - Registrar cambios en el repositorio
+# git commit - Registrar cambios en el repositorio: gti comming -m 
 # git log - Mostrar registros de confirmación
+# git log --graph:  git log --graph --pretty=oneline:
+# git log --graph --decorate --all --oneline
 # git status - Muestra el estado del árbol de trabajo
 # git checkout Cambiar ramas o restaurar archivos de árbol de trabajo
 # git reset Restablecer HEAD actual al estado especificado
+# git alias: git config --global alias.tree "git log --graph --decorate --all --oneline"
