9032727d4701b5fe052102548ad887bc131ced75
SantiTriana <Santi239xll@gmail.com>
2023-03-02 13:56:10 -0500

    Se actualiza el texto del print

--- hellogit.py
+++ hellogit.py
@@ -1,1 +1,9 @@
-print("Hola Git")
+print("New Hello Git")
+
+# git init Para iniciar
+# git add Agrega el contenido del archivo al índice
+# git commit - Registrar cambios en el repositorio
+# git log - Mostrar registros de confirmación
+# git status - Muestra el estado del árbol de trabajo
+# git checkout Cambiar ramas o restaurar archivos de árbol de trabajo
+# git reset Restablecer HEAD actual al estado especificado
